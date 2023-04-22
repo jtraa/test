ab5fd395c4faa7c59ec575fa24d2e35c698ea9ef
Jelle Traa <info@itraa.nl>
2023-04-22 14:42:05 +0200

    Clean up index page

--- public/index.php
+++ public/index.php
@@ -1,52 +1,78 @@
 <?php
 
-require_once '../app/classes/SparePart.php';
-require_once '../app/classes/MaintenanceJob.php';
-require_once '../app/ExternalSystem.php';
 require_once '../vendor/autoload.php';
 
 use App\Classes\ScheduledMaintenanceJob;
 use App\Classes\SparePart;
-use App\ExternalSystem;
 use App\Classes\MaintenanceJob;
+use App\ExternalSystem;
 use Carbon\Carbon;
 
-function createSpareParts()
-{
-    return array(
-        new SparePart('1', 'Engine block', 'Engine'),
-        new SparePart('2', 'Oil filter', 'Filter'),
-        new SparePart('3', 'Air filter', 'Filter'),
-    );
-}
+$externalSystem = new ExternalSystem();
 
-function fetchSparePartPrices($spareParts)
+class App
 {
-    $externalSystem = new ExternalSystem();
-    foreach ($spareParts as $sparePart) {
-        $sparePart->setPrice($externalSystem->getPrice($sparePart->getId()));
+    private array $spareParts;
+    private array $maintenanceJobs;
+
+    public function __construct()
+    {
+        $this->createSpareParts();
+        $this->fetchSparePartPrices();
+        $this->createMaintenanceJobs();
+        $this->run();
+    }
+
+    private function createSpareParts(): void
+    {
+        $this->spareParts = [
+            new SparePart('1', 'Engine block', 'Engine'),
+            new SparePart('2', 'Oil filter', 'Filter'),
+            new SparePart('3', 'Air filter', 'Filter'),
+        ];
+    }
+
+    private function fetchSparePartPrices(): void
+    {
+        global $externalSystem;
+        foreach ($this->spareParts as $sparePart) {
+            $sparePart->setPrice($externalSystem->getPrice($sparePart->getId()));
+        }
+    }
+
+    private function createMaintenanceJobs(): void
+    {
+        $this->maintenanceJobs = [
+            new MaintenanceJob('1', 'Oil change', 'Toyota', 'Corolla', 1.5),
+            new MaintenanceJob('2', 'Engine tune-up', 'Honda', 'Civic', 3.0),
+            new MaintenanceJob('3', 'Brake pad replacement', 'Ford', 'Focus', 2.0),
+        ];
+    }
+
+    private function createScheduledMaintenanceJob(string $brand, string $model, MaintenanceJob $maintenanceJob, array $parts, float $serviceHours, Carbon $startTime, Carbon $endTime): ScheduledMaintenanceJob
+    {
+        return new ScheduledMaintenanceJob($brand, $model, $maintenanceJob, $parts, $serviceHours, $startTime, $endTime);
+    }
+
+    private function printScheduledMaintenanceJobDetails(ScheduledMaintenanceJob $scheduledMaintenanceJob): void
+    {
+        echo 'The price of the scheduled maintenance job is ' . number_format($scheduledMaintenanceJob->getTotalPrice(), 2) . ' USD.' . PHP_EOL;
+    }
+
+    private function run(): void
+    {
+        $scheduledMaintenanceJob = $this->createScheduledMaintenanceJob(
+            'Toyota',
+            'Corolla',
+            $this->maintenanceJobs[0],
+            [$this->spareParts[0], $this->spareParts[1]],
+            2.5,
+            Carbon::parse('2023-05-01 10:00:00'),
+            Carbon::parse('2023-05-01 13:00:00')
+        );
+
+        $this->printScheduledMaintenanceJobDetails($scheduledMaintenanceJob);
     }
 }
 
-function createMaintenanceJobs() {
-    return array(
-        new MaintenanceJob('1', 'Oil change', 'Toyota', 'Corolla', 1.5),
-        new MaintenanceJob('2', 'Engine tune-up', 'Honda', 'Civic', 3.0),
-        new MaintenanceJob('3', 'Brake pad replacement', 'Ford', 'Focus', 2.0),
-    );
-}
-
-function createScheduledMaintenanceJob($brand, $model, $maintenanceJob, $parts, $serviceHours, $startTime, $endTime) {
-    return new ScheduledMaintenanceJob($brand, $model, $maintenanceJob, $parts, $serviceHours, $startTime, $endTime);
-}
-
-function printScheduledMaintenanceJobDetails($scheduledMaintenanceJob) {
-    echo 'The price of the scheduled maintenance job is ' . number_format($scheduledMaintenanceJob->getTotalPrice(), 2) . ' USD.' . PHP_EOL;
-}
-
-
-$spareParts = createSpareParts();
-fetchSparePartPrices($spareParts);
-$maintenanceJobs = createMaintenanceJobs();
-$scheduledMaintenanceJob = createScheduledMaintenanceJob('Toyota', 'Corolla', $maintenanceJobs[0], array($spareParts[0], $spareParts[1]), 2.5, Carbon::parse('2023-05-01 10:00:00'), Carbon::parse('2023-05-01 13:00:00'));
-printScheduledMaintenanceJobDetails($scheduledMaintenanceJob);
+$app = new App();
